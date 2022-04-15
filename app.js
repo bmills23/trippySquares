bed97f3df3153d18e30b5f8ae1df2c2e4e99742c
bmills23 <58231400+bmills23@users.noreply.github.com>
2022-04-15 11:47:41 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-const topDiv = document.querySelector('#top');
-const button = document.querySelector('button')
+
+const button = document.querySelector('button') /*unused code for future interactivity*/
 
 randomInt = (x) => {
-    const rand = Math.floor(Math.random() * x);
+    const rand = Math.floor(Math.random() * x); /*defines a function which will make random number between 0 and x*/
     return rand;
 }
 
-setInterval(function() {
+/*functions below this line are where the magic happens*/ 
+
+
+/*the function below cycles between all 13 squares simultaneously and changes their border colors and backgrounds to a color
+between purple (rgb(255,0,255)) and blue (rgb(0,0,255))
+
+setInterval(function() {  
     for (let i = 1; i <= 13; i++) {
-    const r = randomInt(256)
-    const r2 = randomInt(256)
+    const r = randomInt(256) /*random number needs to be 256 because 0 is included*/
+    const r2 = randomInt(256) /*it's crucial that the border color is changed with a separate # or it'll look to samey*/
     document.getElementById(`${i}`).style.backgroundColor = `rgb(${r},0,255)`
     document.getElementById(`${i}`).style.borderColor = `rgb(${r2},0,255)`
     }
-}, 400)
+}, 400) /*cycles the function every 400 milliseconds*/
+
+/*the function below cycles between the same colors but for the background color of the webpage*/
 
 setInterval(function() {
     const r = randomInt(256)
     document.body.style.backgroundColor = `rgb(${r},0,255)`
-}, 200)
+}, 200) 
