cabf9fb7f040f242c7dfbada400c08020242e963
bmills23 <58231400+bmills23@users.noreply.github.com>
2022-04-15 09:19:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ randomInt = (x) => {
 setInterval(function() {
     for (let i = 1; i <= 13; i++) {
     const r = randomInt(256)
-    // const g = randomInt(256)
-    // const b = randomInt(256)
+    const r2 = randomInt(256)
     document.getElementById(`${i}`).style.backgroundColor = `rgb(${r},0,255)`
-    document.getElementById(`${i}`).style.borderColor = `rgb(${r},0,255)`
+    document.getElementById(`${i}`).style.borderColor = `rgb(${r2},0,255)`
     }
 }, 400)
 
